ad928ec229794704484c3a82582ed2d2e6933870
cat-o-mat <bstefanowa@gmail.com>
2019-12-02 18:24:07 +0100

    day one_2

--- src/com/cat/o/mat/Main.java
+++ src/com/cat/o/mat/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Task dayOne = new Task();
         dayOne.calcFuelRequirements();
+        dayOne.calcFuelRequirementsRec();
     }
 }
 
--- src/com/cat/o/mat/day_one/Task.java
+++ src/com/cat/o/mat/day_one/Task.java
@@ -15,12 +15,47 @@ public class Task {
                     .map(i -> i / 3 - 2)
                     .reduce(Integer::sum)
                     .orElse(0);
-            System.out.println("Day one:\nWhat is the sum of the fuel requirements for all of the modules on your spacecraft? " + wholeWeight);
+            System.out.println("Day one_1:\nWhat is the sum of the fuel requirements for all of the modules on your spacecraft? " + wholeWeight);
             System.out.println("Correct answer is: 3305301");
         } catch (IOException e) {
             System.out.println("Oh noes.");
             e.printStackTrace();
         }
+    }
+
+
+    public void calcFuelRequirementsRec() {
+        try {
+            List<String> inputs = Files.readAllLines(Paths.get("src/com/cat/o/mat/day_one/input.txt"));
+            int wholeWeight = inputs.stream()
+                    .map(Integer::parseInt)
+                    .map(this::calcFuelRec)
+                    .reduce(Integer::sum)
+                    .orElse(0);
+            System.out.println("Day one_2:\nWhat is the sum of the fuel requirements for all of the modules on your spacecraft? " + wholeWeight);
+            System.out.println("Correct answer is: 4955106");
+        } catch (IOException e) {
+            System.out.println("Oh noes.");
+            e.printStackTrace();
+        }
+    }
+
+    private int calcFuelRec(int mass) {
+        int fuel = 0;
+        // initial mass
+        int additionalFuel = mass / 3 - 2;
+        do {
+            fuel += additionalFuel;
+            additionalFuel = additionalFuel / 3 - 2;
+            // additional fuel of max 8 does not require more fuel since
+            // 8 / 3 - 2 = 0
+            // 9 / 3 - 2 = 1
+        } while (additionalFuel > 8);
+        //adding the last calculated fuel
+        if (additionalFuel > 0) {
+            fuel += additionalFuel;
+        }
+        return fuel;
     }
 
 }
